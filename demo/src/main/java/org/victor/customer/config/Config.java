e1174f4c2b234e21fd180df818627926e539fdc9
victor <victor@DESKTOP-8G5KSHD>
2019-10-13 16:19:28 +0200

    Basic (very basic) integration test

--- demo/src/main/java/org/victor/customer/config/Config.java
+++ demo/src/main/java/org/victor/customer/config/Config.java
@@ -1,6 +1,5 @@
 package org.victor.customer.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.github.rozidan.springboot.logger.EnableLogger;
--- demo/src/test/java/org/victor/customer/DemoApplicationTests.java
+++ demo/src/test/java/org/victor/customer/DemoApplicationTests.java
@@ -1,16 +1,41 @@
 package org.victor.customer;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DemoApplicationTests {
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	private HttpHeaders httpHeaders = new HttpHeaders();
+	
+	private String URL="/customers";
 
 	@Test
+	public void addCustomer() {
+		String body="{\"name\":\"First customer\",\"address\":\"First customer address\",\"notes\":\"First customer notes\"}";
+		HttpEntity<String> request = new HttpEntity<String>(body, httpHeaders);
+		
+		ResponseEntity<String> responseEntity = restTemplate.postForEntity(URL, request, String.class);
+		System.out.println(responseEntity.getBody());
+	}
+	
+	@Test
 	public void contextLoads() {
+		
+		String response = restTemplate.getForObject("/customers", String.class, "");
+		
+		System.out.println(response);
 	}
 
 }
